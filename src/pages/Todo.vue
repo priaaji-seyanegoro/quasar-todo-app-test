20ffa1517d997c514a9b297714a1596710935166
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-01-02 15:42:13 +0700

    build function add,delete task

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,5 +1,21 @@
 <template>
   <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        v-model="newTask"
+        class="col"
+        filled
+        placeholder="Add New Task"
+        square
+        bg-color="white"
+        dense
+        @keyup.enter="addNewTask"
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" @click="addNewTask" />
+        </template>
+      </q-input>
+    </div>
     <q-list
       class="bg-white"
       separator
@@ -28,6 +44,13 @@
         </q-item-section>
       </q-item>
     </q-list>
+
+    <div class="div no-task absolute-center" v-if="!tasks.length">
+      <q-icon name="check" size="100px" color="primary" />
+      <div class="div text-h5 text-primary text-center">
+        No Tasks
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -36,19 +59,20 @@ export default {
   name: "Todo",
   data() {
     return {
+      newTask: "",
       tasks: [
-        {
-          title: "Get banana",
-          done: false
-        },
-        {
-          title: "Eat banana",
-          done: false
-        },
-        {
-          title: "Poo banana",
-          done: false
-        }
+        // {
+        //   title: "Get banana",
+        //   done: false
+        // },
+        // {
+        //   title: "Eat banana",
+        //   done: false
+        // },
+        // {
+        //   title: "Poo banana",
+        //   done: false
+        // }
       ]
     };
   },
@@ -63,8 +87,19 @@ export default {
         })
         .onOk(() => {
           this.tasks.splice(index, 1);
-          this.$q.notify("Task Deleted Success");
+          this.$q.notify({
+            message: "Delete successfully",
+            position: "top-right",
+            color: "red"
+          });
         });
+    },
+    addNewTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false
+      });
+      this.newTask = "";
     }
   }
 };
@@ -76,5 +111,8 @@ export default {
     text-decoration: line-through;
     color: #bbb;
   }
+}
+.no-task {
+  opacity: 0.5;
 }
 </style>
